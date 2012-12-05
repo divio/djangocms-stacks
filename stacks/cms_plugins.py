7fd3a8a529d4abd97de96afb488ab847f3bcdbbc
Stefan Foulis <stefan@foulis.ch>
2012-12-05 15:29:59 +0100

    adds cms stack plugin
    closes #2

--- stacks/cms_plugins.py
+++ stacks/cms_plugins.py
@@ -1,22 +1,28 @@
+from django.utils.safestring import mark_safe
 from cms.plugin_pool import plugin_pool
 from cms.plugin_base import CMSPluginBase
 from django.utils.translation import ugettext_lazy as _
-#from stacks.models import StackLink
+from cms.plugin_rendering import render_plugins
+from stacks.models import StackLink
 from cms.plugins.utils import get_plugins
 
-#class StackPlugin(CMSPluginBase):
-#    model = StackLink
-#    name = _("Stack")
-#
-#    render_template = "cms/plugins/stacks.html"
-#
-#    def render(self, context, instance, placeholder):
-#        plugins = get_plugins(context['request'], instance.stack.content)
-#        context.update({
-#            'object': instance,
-#            'placeholder':placeholder,
-#            'plugins':plugins,
-#        })
-#        return context
-#
-#plugin_pool.register_plugin(StackPlugin)
+class StackPlugin(CMSPluginBase):
+    model = StackLink
+    name = _("Stack")
+
+    render_template = "cms/plugins/stacks.html"
+
+    def render(self, context, instance, placeholder):
+        # TODO: once we drop 2.3.x support we can just use the "render_plugin" templatetag
+        #       instead of rendering html here.
+        plugins = get_plugins(context['request'], instance.stack.content)
+        processors = ()
+        html_content = mark_safe("".join(render_plugins(plugins, context, placeholder, processors)))
+        context.update({
+            'instance': instance,
+            'placeholder': placeholder,
+            'content': html_content,
+        })
+        return context
+
+plugin_pool.register_plugin(StackPlugin)
